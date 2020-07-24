9c08c005218e2cafb4ceaa296f6fe34695ba9f00
Ryo-0729 <ryo0810hato@icloud.com>
2020-07-24 15:18:37 +0900

    commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, :lastname, :firstname, :lastname_kana, :firstname_kana, :birth_day, :tel_num, :send_lastname, :send_firstname, :send_lastname_kana, :send_firstname_kana, :post_num, :prefectures, :city, :address, :address_2nd,presence: true
+end
