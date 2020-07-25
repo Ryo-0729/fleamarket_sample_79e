f15e8790e5878256824ef2da3c58de0c192fcb11
fujiki1987 <67051297+fujiki1987@users.noreply.github.com>
2020-07-25 19:32:45 +0900

    user-new commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'messages#index'
+  resources :messages, only: [:index] do
+    collection do
+      get 'index2', 'index3', 'index4', 'index5'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
