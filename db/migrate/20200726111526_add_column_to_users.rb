8ef7ef571d44a3e0c1c2fa0141168403c08ea94e
Ryo-0729 <ryo0810hato@icloud.com>
2020-07-27 20:03:38 +0900

    commit

---
+++ db/migrate/20200726111526_add_column_to_users.rb
@@ -0,0 +1,24 @@
+class AddColumnToUsers < ActiveRecord::Migration[6.0]
+  def change
+    # add_column :users, :last_name, :string
+    # add_column :users, :first_name, :string
+    # add_column :users, :last_name_kana, :string
+    # add_column :users, :first_name_kana, :string
+    # add_column :users, :birthyear, :string
+    # add_column :users, :birthmonth, :string
+    # add_column :users, :birthday, :string
+    # add_column :users, :telephone, :string
+    # add_column :users, :encrypted_password, :string
+    # add_column :users, :icon, :text
+    # add_column :users, :text, :text
+    # add_column :users, :post_cord, :integer
+    # add_column :users, :city, :string
+    # add_column :users, :address, :string
+    # add_column :users, :building, :text
+    # add_column :users, :prefecture_id, :string
+    # add_column :users, :user, :references
+    # add_column :users, :reset_password_token, :string
+    # add_column :users, :reset_password_sent_at, :datetime
+    # add_column :users, :remember_created_at, :datetaime
+  end
+end
