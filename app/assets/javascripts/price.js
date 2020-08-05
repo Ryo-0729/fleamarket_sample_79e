80da36f76330e648477a414a89342fbbefc9335d
yassan-h <yasuhiro.mpr@gmail.com>
2020-08-05 18:37:59 +0900

    add en-symbol

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -5,7 +5,7 @@ $(function(){
     let mfSalesFee = Math.floor(salesFee);
     let salesProfit = price * 0.9;
     let mcSalesProfit = Math.ceil(salesProfit);
-  $('#SalesFee').text(mfSalesFee);
-  $('#SalesProfit').text(mcSalesProfit);
+  $('#SalesFee').text('¥' + mfSalesFee);
+  $('#SalesProfit').text('¥' + mcSalesProfit);
   });
 });
