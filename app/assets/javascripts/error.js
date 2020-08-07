65b52b770bcc3258ed2b57121d5ec9286a708a3a
yassan-h <yasuhiro.mpr@gmail.com>
2020-08-07 14:28:20 +0900

    edit error.js

---
+++ app/assets/javascripts/error.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.Button__Form__Red').submit(function(){
+    $('.upper-box')
+    if($('.upper-box').val() == ""){
+    return false;
+    }
+  });
+});
+
+
+var img = document.getElementById('myimage');
+var src = myimage.getAttribute('src');
